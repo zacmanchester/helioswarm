d9dae328b3b21a023461bd3986c55f37d85235bf
Zac Manchester <zacmanchester@stanford.edu>
2018-07-26 12:35:46 -0700

    Single sat iekf working

--- filtersim.m
+++ filtersim.m
@@ -24,7 +24,7 @@ p = a*(1-e*e); %semilatus rectum
 x0 = [r0/1000; v0*(24*60*60/1000)];
 t0 = 25 + 22/24 + 30/(24*60) + 25.52/(24*60*60);
 
-t_samp = t0:(10/24/60):(t0+14); %once every 10 minutes
+t_samp = t0:(15/24/60):(t0+14); %once every 15 minutes
 
 Nsat = 2;
 x0swarm = [eye(6); diag(ones(6,1)+[1e-4*randn(3,1); 1e-5*randn(3,1)])]*x0; %; diag(ones(6,1)+[1e-4*randn(3,1); 1e-5*randn(3,1)]); diag(ones(6,1)+[1e-4*randn(3,1); 1e-5*randn(3,1)])]*x0;
@@ -35,79 +35,87 @@ tic
 xswarm = xswarm';
 toc
 
-yhist = zeros(Nsat+2,length(t));
+xhub = xswarm(1:6,:);
+xnode = xswarm(7:12,:);
+yhist = zeros(1,length(t));
 for k = 1:length(t)
-    yhist(:,k) = observation(xswarm(:,k));
+    yhist(:,k) = observation(xnode(:,k),xhub(:,k)) + 2e-5*randn;
 end
 
-P0 = 1e-3*eye(12);
-Q = (1e-6)*eye(12);
-R = diag([1e-6, 1e-6, 1e-6, 1e-14]);
 
-%[xhist,Phist] = iekf([x0+randn(6,1); x0+randn(6,1)],P0,Q,R,t,yhist,t_ephem,x_ephem);
-[xhist,Phist] = iukf([x0+randn(6,1); x0+randn(6,1)],P0,Q,R,t,yhist,t_ephem,x_ephem);
+P0 = 1e-3*eye(6);
+Q = 1e-5*eye(6);
+R = 1e-8;
 
-figure();
-subplot(3,2,1);
-plot(t,1000*(xhist(1,:)-xswarm(1,:)));
-hold on
-plot(t,1000*sqrt(squeeze(Phist(1,1,:))),'r');
-plot(t,-1000*sqrt(squeeze(Phist(1,1,:))),'r');
-subplot(3,2,3);
-plot(t,1000*(xhist(2,:)-xswarm(2,:)));
-hold on
-plot(t,1000*sqrt(squeeze(Phist(2,2,:))),'r');
-plot(t,-1000*sqrt(squeeze(Phist(2,2,:))),'r');
-subplot(3,2,5);
-plot(t,1000*(xhist(3,:)-xswarm(3,:)));
-hold on
-plot(t,1000*sqrt(squeeze(Phist(3,3,:))),'r');
-plot(t,-1000*sqrt(squeeze(Phist(3,3,:))),'r');
-subplot(3,2,2);
-plot(t,(xhist(4,:)-xswarm(4,:)));
-hold on
-plot(t,sqrt(squeeze(Phist(4,4,:))),'r');
-plot(t,-sqrt(squeeze(Phist(4,4,:))),'r');
-subplot(3,2,4);
-plot(t,(xhist(5,:)-xswarm(5,:)));
-hold on
-plot(t,sqrt(squeeze(Phist(5,5,:))),'r');
-plot(t,-sqrt(squeeze(Phist(5,5,:))),'r');
-subplot(3,2,6);
-plot(t,(xhist(6,:)-xswarm(6,:)));
-hold on
-plot(t,sqrt(squeeze(Phist(6,6,:))),'r');
-plot(t,-sqrt(squeeze(Phist(6,6,:))),'r');
+[xhist,Phist] = iekf(x0+0.0001*randn(6,1),P0,Q,R,t,yhist,xhub,t_ephem,x_ephem);
 
-figure();
-subplot(3,2,1);
-plot(t,1000*(xhist(7,:)-xswarm(7,:)));
+figure(1);
+subplot(3,1,1);
+plot(t,1000*(xhist(1,:)-xnode(1,:)));
 hold on
-plot(t,1000*sqrt(squeeze(Phist(7,7,:))),'r');
-plot(t,-1000*sqrt(squeeze(Phist(7,7,:))),'r');
-subplot(3,2,3);
-plot(t,1000*(xhist(8,:)-xswarm(8,:)));
+plot(t,2000*sqrt(squeeze(Phist(1,1,:))),'r');
+plot(t,-2000*sqrt(squeeze(Phist(1,1,:))),'r');
+subplot(3,1,2);
+plot(t,1000*(xhist(2,:)-xnode(2,:)));
 hold on
-plot(t,1000*sqrt(squeeze(Phist(8,8,:))),'r');
-plot(t,-1000*sqrt(squeeze(Phist(8,8,:))),'r');
-subplot(3,2,5);
-plot(t,1000*(xhist(9,:)-xswarm(9,:)));
+plot(t,2000*sqrt(squeeze(Phist(2,2,:))),'r');
+plot(t,-2000*sqrt(squeeze(Phist(2,2,:))),'r');
+ylabel('Position Error (km)');
+subplot(3,1,3);
+plot(t,1000*(xhist(3,:)-xnode(3,:)));
 hold on
-plot(t,1000*sqrt(squeeze(Phist(9,9,:))),'r');
-plot(t,-1000*sqrt(squeeze(Phist(9,9,:))),'r');
-subplot(3,2,2);
-plot(t,(xhist(10,:)-xswarm(10,:)));
+plot(t,2000*sqrt(squeeze(Phist(3,3,:))),'r');
+plot(t,-2000*sqrt(squeeze(Phist(3,3,:))),'r');
+xlabel('Time (days)');
+
+figure(2)
+subplot(3,1,1);
+plot(t,(xhist(4,:)-xnode(4,:)));
 hold on
-plot(t,sqrt(squeeze(Phist(10,10,:))),'r');
-plot(t,-sqrt(squeeze(Phist(10,10,:))),'r');
-subplot(3,2,4);
-plot(t,(xhist(5,:)-xswarm(11,:)));
+plot(t,2*(1e6/86400)*sqrt(squeeze(Phist(4,4,:))),'r');
+plot(t,-2*(1e6/86400)*sqrt(squeeze(Phist(4,4,:))),'r');
+subplot(3,1,2);
+plot(t,(xhist(5,:)-xnode(5,:)));
 hold on
-plot(t,sqrt(squeeze(Phist(11,11,:))),'r');
-plot(t,-sqrt(squeeze(Phist(11,11,:))),'r');
-subplot(3,2,6);
-plot(t,(xhist(12,:)-xswarm(12,:)));
+plot(t,2*(1e6/86400)*sqrt(squeeze(Phist(5,5,:))),'r');
+plot(t,-2*(1e6/86400)*sqrt(squeeze(Phist(5,5,:))),'r');
+ylabel('Velocity Error (m/s)');
+subplot(3,1,3);
+plot(t,(xhist(6,:)-xnode(6,:)));
 hold on
-plot(t,sqrt(squeeze(Phist(12,12,:))),'r');
-plot(t,-sqrt(squeeze(Phist(12,12,:))),'r');
+plot(t,2*(1e6/86400)*sqrt(squeeze(Phist(6,6,:))),'r');
+plot(t,-2*(1e6/86400)*sqrt(squeeze(Phist(6,6,:))),'r');
+xlabel('Time (days)');
+
+% figure();
+% subplot(3,2,1);
+% plot(t,1000*(xhist(7,:)-xswarm(7,:)));
+% hold on
+% plot(t,1000*sqrt(squeeze(Phist(7,7,:))),'r');
+% plot(t,-1000*sqrt(squeeze(Phist(7,7,:))),'r');
+% subplot(3,2,3);
+% plot(t,1000*(xhist(8,:)-xswarm(8,:)));
+% hold on
+% plot(t,1000*sqrt(squeeze(Phist(8,8,:))),'r');
+% plot(t,-1000*sqrt(squeeze(Phist(8,8,:))),'r');
+% subplot(3,2,5);
+% plot(t,1000*(xhist(9,:)-xswarm(9,:)));
+% hold on
+% plot(t,1000*sqrt(squeeze(Phist(9,9,:))),'r');
+% plot(t,-1000*sqrt(squeeze(Phist(9,9,:))),'r');
+% subplot(3,2,2);
+% plot(t,(xhist(10,:)-xswarm(10,:)));
+% hold on
+% plot(t,sqrt(squeeze(Phist(10,10,:))),'r');
+% plot(t,-sqrt(squeeze(Phist(10,10,:))),'r');
+% subplot(3,2,4);
+% plot(t,(xhist(5,:)-xswarm(11,:)));
+% hold on
+% plot(t,sqrt(squeeze(Phist(11,11,:))),'r');
+% plot(t,-sqrt(squeeze(Phist(11,11,:))),'r');
+% subplot(3,2,6);
+% plot(t,(xhist(12,:)-xswarm(12,:)));
+% hold on
+% plot(t,sqrt(squeeze(Phist(12,12,:))),'r');
+% plot(t,-sqrt(squeeze(Phist(12,12,:))),'r');
 
--- iekf.m
+++ iekf.m
@@ -1,4 +1,4 @@
-function [xhist,Phist] = iekf(x0,P0,Q,R,thist,yhist,t_ephem,x_ephem)
+function [xhist,Phist] = iekf(x0,P0,Q,R,thist,yhist,xhub,t_ephem,x_ephem)
 
 %Setup
 Nx = length(x0);
@@ -9,8 +9,8 @@ dt = thist(2)-thist(1); %assume uniform sampling
 %First step
 xnew = x0;
 dx = 1;
-while any(abs(dx) > 1e-5)
-    [yp, C] = observation(xnew);
+while any(abs(dx) > 1e-6)
+    [yp, C] = observation(xnew,xhub(:,1));
     
     %Innovation
     z = yhist(:,1) - yp;
@@ -29,13 +29,13 @@ Phist(:,:,1) = (eye(Nx)-K*C)*P0*(eye(Nx)-K*C)' + K*R*K';
 for k = 1:(length(thist)-1)
     
     %Predict
-    [xp, A] = ode2(thist(k),xhist(:,k));
+    [xp, A] = ode3(thist(k),xhist(:,k));
     Pp = A*Phist(:,:,k)*A' + Q;
     
     xnew = xp;
     dx = 1;
-    while any(abs(dx) > 1e-5)
-        [yp, C] = observation(xnew);
+    while any(abs(dx) > 1e-6)
+        [yp, C] = observation(xnew,xhub(:,k+1));
         
         %Innovation
         z = yhist(:,k+1) - yp;
@@ -56,11 +56,19 @@ for k = 1:(length(thist)-1)
 end
 
     function [xn, A] = ode2(t,x)
-        [xdot1,A1] = swarm_dynamics(t,x,t_ephem,x_ephem);
+        [xdot1,A1] = dynamics(t,x,t_ephem,x_ephem);
         xmid = x + (dt/2)*xdot1;
-        [xdot2,A2] = swarm_dynamics(t+dt/2,xmid,t_ephem,x_ephem);
+        [xdot2,A2] = dynamics(t+dt/2,xmid,t_ephem,x_ephem);
         xn = x + dt*xdot2;
         A = eye(Nx) + dt*A2 + 0.5*dt*dt*A2*A1;
+    end
+
+    function [xn, A] = ode3(t,x)
+        [xdot1,A1] = dynamics(t,x,t_ephem,x_ephem);
+        [xdot2,A2] = dynamics(t+dt/2,x + (dt/2)*xdot1,t_ephem,x_ephem);
+        [xdot3,A3] = dynamics(t+dt,x - dt*xdot1 + 2*dt*xdot2,t_ephem,x_ephem);
+        xn = x + (dt/6)*(xdot1 + 4*xdot2 + xdot3);
+        A = eye(Nx) + (dt/6)*(A1+4*A2+A3) + (dt*dt/6)*(2*A2*A1-A3*A1+2*A3*A2) + (dt*dt*dt/6)*A3*A2*A1;
     end
 
 end
